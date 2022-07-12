8aa6eafc6893465e481182faaabab3442ee57982
Phillip S <Phillip.soetebeer@gmail.com>
2022-07-11 22:13:25 -0400

    Testing Inheritance

---
+++ @garys_tweaks/addons/garys_tweaks/Config_Phil.cpp
@@ -0,0 +1,80 @@
+class CfgPatches
+{
+	class Garys_tweaks
+	 	{
+ 		units[] = {};
+ 		weapons[] = {};
+ 		requiredVersion = 0.1;
+ 		requiredAddons[] = 
+		{	"ace_ballistics",
+			"ace_weather",
+			"rhsusf_main",
+			"rhs_main",
+			"rhsgref_main",
+			"rhssaf_main",
+			"rhsusf_main",
+			"UK3CB_BAF_Weapons",
+			"UK3CB_BAF_Equipment",
+			"UK3CB_BAF_Weapons_RHS_ammo_compatibility_",
+			"hlc_core",
+			"hlc_rhs_compat_CORE",
+			"hlc_ace3_Compat_CORE",
+			"ace_compat_rhs_afrf3",
+			"ace_compat_rhs_gref3",
+			"ace_compat_rhs_saf3",
+			"ace_compat_rhs_usf3"
+			
+		};
+
+ 	};
+};
+class CfgAmmo
+{
+		class B_556x45_Ball;
+
+		class rhs_ammo_556x45_M855A1_Ball: B_556x45_Ball
+	{
+	};
+	class rhs_ammo_556x45_M855A1_Ball_Red: rhs_ammo_556x45_M855A1_Ball
+	{
+	};
+	class rhs_ammo_556x45_M855A1_Ball_Green: rhs_ammo_556x45_M855A1_Ball_Red
+	{
+	};
+	class rhs_ammo_556x45_M855A1_Ball_Yellow: rhs_ammo_556x45_M855A1_Ball_Red
+	{
+	};
+	class rhs_ammo_556x45_M855A1_Ball_Orange: rhs_ammo_556x45_M855A1_Ball_Red
+	{
+	};
+	class rhs_ammo_556x45_M855_Ball: B_556x45_Ball
+	{
+	};
+	class rhs_ammo_556x45_M855_Ball_Red: rhs_ammo_556x45_M855_Ball
+	{
+	};
+	class rhs_ammo_556x45_M855_Ball_Green: rhs_ammo_556x45_M855_Ball_Red
+	{
+	};
+	class rhs_ammo_556x45_M855_Ball_Yellow: rhs_ammo_556x45_M855_Ball_Red
+	{
+	};
+	class rhs_ammo_556x45_M855_Ball_Orange: rhs_ammo_556x45_M855_Ball_Red
+	{
+	};
+	class rhs_ammo_556x45_Mk318_Ball: B_556x45_Ball
+	{
+	};
+	class rhs_ammo_556x45_Mk262_Ball: B_556x45_Ball
+	{
+	};
+	class rhs_ammo_556x45_M193_Ball: B_556x45_Ball
+	{
+	};
+	class rhs_ammo_556x45_M196_Ball_Red: rhs_ammo_556x45_M193_Ball
+	{
+	};
+	class rhs_ammo_556x45_M995_AP: B_556x45_Ball
+	{
+	};
+};
